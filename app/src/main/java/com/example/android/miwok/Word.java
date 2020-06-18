d7d7fd6b7521c6d96eb05004e27dc381db4ca82a
Jofiel Batista <jofielbatista@hotmail.com>
2020-06-18 16:14:53 -0600

    I changed the word adapter to put the correct image

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -4,10 +4,23 @@ public class Word {
 
     private String mMiwokWord;
     private String mTranslatedWord;
+    private int mImageIdResource;
 
     public Word(String miwokWord, String translationWord){
         mMiwokWord = miwokWord;
         mTranslatedWord = translationWord;
+    }
+    /*
+    * Create a new Word Object
+    * @param miwokWord  Is the word in the miwok language
+    * @param translationWord  Is the word in the language we want to make the translation
+    * @param imageResourceId  Is the resource of the image that represents the word
+    *
+    * */
+    public Word(String miwokWord, String translationWord, int imageResourceId){
+        mMiwokWord = miwokWord;
+        mTranslatedWord = translationWord;
+        mImageIdResource = imageResourceId;
     }
     public String getMiwokWord() {
         return mMiwokWord;
@@ -16,5 +29,7 @@ public class Word {
     public String getTranslatedWord() {
         return mTranslatedWord;
     }
+
+    public int getImageIdResource(){ return mImageIdResource;}
 
 }
--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -28,6 +29,9 @@ public class WordAdapter extends ArrayAdapter<Word> {
 
         Word currentWord = getItem(position);
 
+        ImageView imageView = (ImageView) listItemView.findViewById(R.id.image);
+        imageView.setImageResource(currentWord.getImageIdResource());
+
         TextView miwokText = (TextView) listItemView.findViewById(R.id.txtMiwokhWord);
         TextView englishText = (TextView) listItemView.findViewById(R.id.txtEnglishWord);
 
@@ -36,4 +40,6 @@ public class WordAdapter extends ArrayAdapter<Word> {
 
         return listItemView;
     }
+
+
 }
