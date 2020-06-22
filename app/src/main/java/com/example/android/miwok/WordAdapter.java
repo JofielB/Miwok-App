d6dda4acd0c40b58da8fad886bb61c3e5c6dbf19
Jofiel Batista <jofielbatista@hotmail.com>
2020-06-22 14:56:45 -0600

    More visual polish

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -3,25 +3,29 @@ package com.example.android.miwok;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class WordAdapter extends ArrayAdapter<Word> {
 
-    public WordAdapter(Context context, ArrayList<Word> wordsList){
+    private int mBackgroundColor;
+    public WordAdapter(Context context, ArrayList<Word> wordsList, int backgroundColor){
         super(context, 0, wordsList);
-
+        mBackgroundColor = backgroundColor;
     }
 
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
         View listItemView = convertView;
         if(listItemView == null){
             listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_itemt, parent, false);
@@ -40,6 +44,11 @@ public class WordAdapter extends ArrayAdapter<Word> {
 
         miwokText.setText(currentWord.getMiwokWord());
         englishText.setText(currentWord.getTranslatedWord());
+
+        View texBox = listItemView.findViewById(R.id.textBox);
+        //We need to convert the resource id of the color into a color
+        int color = ContextCompat.getColor(getContext(), mBackgroundColor);
+        texBox.setBackgroundColor(color);
 
         return listItemView;
     }
